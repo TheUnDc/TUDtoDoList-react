ad75455b819f72e7d807eda16141020274d7da45
Tomasz Pietruczuk <tomekpietruczuk3@gmail.com>
2023-08-19 15:11:59 +0200

    added hidedone function to hideable button

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
+import { useState } from "react";
 
 const tasks = [
   { id: 1, content: "suoer smart", done: false },
   { id: 2, content: "note surtttt", done: true }
 ];
 
-const hideDone = false;
-
 function App() {
+
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
   return (
     <Container>
-      <Header 
-        title={`Lista zadań`} 
+      <Header
+        title={`Lista zadań`}
       />
       <Section
         title={`Dodaj nowe zadanie`}
@@ -25,7 +30,7 @@ function App() {
       <Section
         title={`Lista zadań`}
         specialAddons={<Tasks tasks={tasks} hideDone={hideDone} />}
-        headerAddons={<Buttons tasks={tasks} hideDone={hideDone} />}
+        headerAddons={<Buttons tasks={tasks} hideDone={hideDone} toggleHideDone={toggleHideDone} />}
       />
     </Container>
   )

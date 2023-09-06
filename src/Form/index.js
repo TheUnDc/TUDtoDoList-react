18e794c9bfe9c7a0abe8fd34a52ad01bf9964582
Tomasz Pietruczuk <tomekpietruczuk3@gmail.com>
2023-09-06 15:49:43 +0200

    added focus on submit after render

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,9 +1,12 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./style.css";
 
 const Form = ({addNewTask}) => {
 
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef();
+
+    const focusInput = () => {inputRef.current.focus()} 
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -13,12 +16,14 @@ const Form = ({addNewTask}) => {
 
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
+        focusInput();
     }
 
     return (
         <form className="form" onSubmit={onFormSubmit}>
             <input
                 value={newTaskContent}
+                ref={inputRef}
                 autoFocus
                 placeholder="Co jest do zrobienia?"
                 className="form__input"

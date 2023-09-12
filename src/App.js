823c676239c38218b2110e897d1b5e84288b0068
Tomasz Pietruczuk <tomekpietruczuk3@gmail.com>
2023-09-12 23:16:56 +0200

    maked useHide hook and exported useTasks and useHide to separate jsfile

--- src/App.js
+++ src/App.js
@@ -4,62 +4,19 @@ import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
-import { useState, useEffect } from "react";
-
-
-const useTasks = () => {
-
-  const tasksStorageCheck = () => JSON.parse(localStorage.getItem("tasks")) || [];
-  const [tasks, setTasks] = useState(tasksStorageCheck);
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks))
-  }, [tasks]);
-
-  const removeTask = (id) => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
-
-  const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => task.id === id ? { ...task, done: !task.done } : task));
-  };
-
-  const setAllDone = () => {
-    setTasks(tasks => tasks.map(task => ({ ...task, done: true })));
-  };
-
-  const addNewTask = (newTaskContent) => {
-    setTasks(tasks => [...tasks, {
-      content: newTaskContent,
-      done: false,
-      id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-    },
-    ])
-  };
-
-  return ([tasks, removeTask, toggleTaskDone, setAllDone, addNewTask]);
-
-};
+import { useTasks, useHide } from "./useCustomState";
 
 function App() {
 
   const [
-    tasks, 
+    tasks,
     removeTask,
     toggleTaskDone,
     setAllDone,
     addNewTask,
   ] = useTasks();
 
-  const hideStorageCheck = () => JSON.parse(localStorage.getItem("hide")) || false;
-  const [hideDone, setHideDone] = useState(hideStorageCheck);
-
-  useEffect(() => {
-    localStorage.setItem("hide", JSON.stringify(hideDone))
-  }, [hideDone]);
-
-  const toggleHideDone = () => {
-    setHideDone(hideDone => !hideDone);
-  };
+  const [hideDone, toggleHideDone] = useHide();
 
   return (
     <Container>
